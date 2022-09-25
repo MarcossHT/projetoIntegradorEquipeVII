bf9253612c28ce819683bb15cfa0dd8d65f67817
Marcos Pires <80723606+MarcossHT@users.noreply.github.com>
2022-09-25 16:14:04 -0300

    feat: delete repository

--- trabalhoIntegradorEquipeVII/src/main/java/clinicaOdontologica/trabalhoIntegradorEquipeVII/repository/ITipoUsuarioRepository.java
+++
@@ -1,7 +0,0 @@
-package clinicaOdontologica.trabalhoIntegradorEquipeVII.repository;
-
-import clinicaOdontologica.trabalhoIntegradorEquipeVII.model.TipoUsuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ITipoUsuarioRepository extends JpaRepository<TipoUsuario, Integer> {
-}
